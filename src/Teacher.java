ac5aac95055e75961957af9112b92de77457162b
BT_1N3_27 <hkaur0745@bths.edu>
2018-10-22 14:48:20 -0400

    update

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,2 +1,14 @@
-public class Teacher {
+public class Teacher extends Person {
+    private String Subject;
+    private String familyName;
+
+    public Teacher(String Subject, String familyName) {
+        super(Subject, familyName);
+
+
+        public String
+    }
 }
+
+
+
