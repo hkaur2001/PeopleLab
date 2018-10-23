82477012a3657d0c5bf8ea0d5768a9558886a2a1
BT_1N3_26 <ljaoutakas8934@bths.edu>
2018-10-23 14:40:07 -0400

    I dont want a message

--- src/Student.java
+++ src/Student.java
@@ -2,14 +2,35 @@ public class Student extends Person
 {
     private String firstName;
     private String familyName;
-    public Student(String firstName, String familyName)
+    private double GPA;
+    private double osis;
+    private double gradeLevel;
+    public Student(String firstName, String familyName, double GPA, double osis, double gradeLevel)
     {
         super(firstName, familyName);
         this.firstName = firstName;
-    }
-    public double getGpa()
-    {
-        return (5);
+        this.familyName = familyName;
+        this.GPA = GPA;
+        this.osis = osis;
+        this.gradeLevel = gradeLevel;
     }
 
+    public double getGpa()
+    {
+        return GPA;
+    }
+
+    public double getOsis()
+    {
+        return osis;
+    }
+
+    public double getGradeLevel()
+    {
+        return gradeLevel;
+    }
+    public String toString()
+    {
+        return (firstName + ", " + familyName);
+    }
 }
