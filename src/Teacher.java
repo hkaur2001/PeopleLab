dce2825f2161b7fe145c1a40ec645b6e4172f057
BT_1N3_27 <hkaur0745@bths.edu>
2018-10-23 14:43:28 -0400

    teacher update

--- src/Teacher.java
+++ src/Teacher.java
@@ -1,13 +1,27 @@
 public class Teacher extends Person {
     private String Subject;
     private String familyName;
+    private String Title;
 
-    public Teacher(String Subject, String familyName) {
+    public Teacher(String Subject, String familyName, String Title) {
         super(Subject, familyName);
-
-
-        public String
+        this.Subject = Subject;
+        this.familyName=familyName;
+        this.Title= Title;
     }
+    public String getSubject() {
+        return (Subject);
+    }
+    public String familyName() {
+        return (familyName);
+    }
+    public String Title(){
+        return(Title);
+    }
+    public String toString(){
+        return Title + ". " + familyName;
+    }
+
 }
 
 
